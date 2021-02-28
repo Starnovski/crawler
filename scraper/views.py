a0f520cc36d8ff079d0eef34fe0daca0f5574f25
Starnovski <pawelsz1818@wp.pl>
2021-02-28 15:08:16 +0100

    Parsing HTML and searching for links

--- scraper/views.py
+++ scraper/views.py
@@ -3,9 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 from .forms import CrawlerForm
 
-# Create your views here.
+# That code probably shouldn't even been pushed on GitHub. I intended to make an app
+# without a model, just for practice, so decided to make a crawler, which should
+# return a list of links. And it works, but only for links between the <li> tag.
+# Definitely won't work in many cases. It's obviously too simple, and I'm not going to
+# work on it anymore, for it is only an excercise.
+
 def show_list(request):
-    alert = "First info"
+    alert = "Paste a link to a website you want to search"
 
     if request.method=='POST':
         form = CrawlerForm(request.POST)
@@ -17,7 +22,21 @@ def show_list(request):
                 data = requests.get(data)
             except:
                 data = "Can't connect"
-            return render(request,"index.html",{'alert':alert, 'data':data})
+
+            plain = data.content
+            results = BeautifulSoup(plain, "html.parser")
+            #Finding all results with given tag
+            links = results.findAll('li')
+            #Initializing an empty list of items
+            items=[]
+            for link in links:
+                a = link.find('a',href=True)
+                if a is None:
+                    continue
+                else:
+                    a = a['href']
+                    items.append(a)
+            return render(request,"index.html",{'alert':alert, 'data':data, 'items':items})
     else:
         form = CrawlerForm()
     return render(request,"index.html",{'alert':alert, 'form':form})
